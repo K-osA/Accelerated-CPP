cbc9906bfbef564f724ca630e30cf9b67c56a5a4
Sangwon Ko <evbayooooo@gmail.com>
2020-07-18 13:06:47 +0000

    2020-07-18 준비

--- CH-4/practice/6/grade.cpp
+++ CH-4/practice/6/grade.cpp
@@ -7,7 +7,7 @@
 using std::domain_error;
 using std::vector;
 
-double grade(double midterm, double final, const vector<double>& hw)
+double grade(double midterm, double final, vector<double>& hw)
 {
 	if(hw.size()==0)
 		throw domain_error("student has done no homework");
--- CH-4/practice/6/grade.h
+++ CH-4/practice/6/grade.h
@@ -5,6 +5,6 @@
 #include "Student_info.h"
 
 double grade(double, double, double);
-double grade(double, double, const std::vector<double>&);
+double grade(double, double, std::vector<double>&);
 
 #endif
